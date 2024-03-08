4e78df930dca2ff90b3e4c0fed0983bcd8d68518
Manuel <manuelguvi500@gmail.com>
2024-03-08 00:23:58 -0500

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,5 @@
 function skillsMember() {
-    return {
-        name: 'John',
-        age: 30,
-        skills: ['HTML', 'CSS', 'JS'],
-        getSkills: function () {
-            return this.skills;
-        }
-    };
+    return 'Member skills: ' + this.skills.join(', ');
 }
 
 
