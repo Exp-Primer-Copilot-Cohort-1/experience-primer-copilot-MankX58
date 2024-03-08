e58ed6dd7f4885c3341915e729f19013ec5a1eb1
Manuel <manuelguvi500@gmail.com>
2024-03-08 00:17:52 -0500

    John

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        getSkills: function () {
+            return this.skills;
+        }
+    };
+}
+
+
